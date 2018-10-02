a024fc4d5c7f9f99fd7dd9f79458c2501e8c4391
eimy18 <43798083+eimy18@users.noreply.github.com>
2018-10-02 16:21:18 -0700

    codigo eimy
    prueba eimy

---
+++ JavaBdGitHub2-master/src/javabdgithub/JavaBdGitHub.java
@@ -0,0 +1,19 @@
+package javabdgithub;
+
+public class JavaBdGitHub {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hola Java BD GitHub");
+        System.out.println("Hola chicos del proyecto");
+        
+        int v1 = 2;        
+        int v2 = 2;
+        int suma = v1 + v2;
+        
+        System.out.println("Suma de valores "+suma);
+        System.out.println("sdf");
+        
+    }
+    
+}
